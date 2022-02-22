92e0f9843471c3e62c22b55a01bd71a202562a46
neeraj00890 <kumar74312@gmail.com>
2022-02-22 09:51:17 +0530

    code

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,33 +9,32 @@ const tokenVerifer =  require("./services/token-verifier")
 const unProtectedRoutes = [
     "/api/login",
     "/api/register",
-    "/"
 ]
 app.use(cors({
     origin:"*"
 }))
-app.use(express.static(path.join(__dirname,'/public')))
-app.use((req, res, next) => {
+app.use(express.static(path.join(__dirname, 'public')))
+// app.use((req, res, next) => {
     
-    if(!unProtectedRoutes.includes(req.url)) {
-        console.log(req.url)
-        const tokenHeader = req.headers.authorization || "";
-        const token = tokenHeader.substring(7);
-        const userInfo = tokenVerifer.tokenVerifer(token);
+//     if(!unProtectedRoutes.includes(req.url) || req.url.includes("\\")) {
+//         console.log(req.url)
+//         const tokenHeader = req.headers.authorization || "";
+//         const token = tokenHeader.substring(7);
+//         const userInfo = tokenVerifer.tokenVerifer(token);
    
-        if(userInfo) {
-            req.loggedInEmail = userInfo.Email;
-            next();
-        } else {
-            return res.status(401).send({message: "Unauthorized"})
-        }
-    } else {
-        next();
-    }
+//         if(userInfo) {
+//             req.loggedInEmail = userInfo.Email;
+//             next();
+//         } else {
+//             return res.status(401).send({message: "Unauthorized"})
+//         }
+//     } else {
+//         next();
+//     }
     
 
 
-})
+// })
 
 app.use(express.json());
 app.use(authController);  
